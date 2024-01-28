7ce060ad4eeed6927cd98e42cb6864650a5bb697
JD-janydaniel <janydaniel59@gmail.com>
2024-01-28 22:21:34 +0530

    day03 task

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 // Question no:2
 // Use the rest countries API URL->https://restcountries.com/v3.1/all and diplay all the contries flag in the console
 // Solution
+var request = new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all");
+request.send();
+request.onload = function(){
+    var result = JSON.parse(request.response);
+    console.log(result);
+    for(var i=0;i<result.length;i++){
+        console.log(result[i].flags.png);
+    }
+}
+
+//Question no:3
+//Use the same rest countries API and print all countries names,regions,sub-region and population
+//Solution
+var request = new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all");
+request.send();
+request.onload = function(){
+    var res = JSON.parse(request.response);
+    console.log(res);
+    for(var i=0;i<res.length;i++){
+        if(res[i].subregion){
+        console.log("common name "+res[i].name.common+" region "+res[i].region+" subregion "+res[i].subregion+" population "+res[i].population);
+    }
+    else{
+        console.log("common name "+res[i].name.common+" region "+res[i].region+ " no subregion"+" population "+res[i].population);
+    }
+}
+}

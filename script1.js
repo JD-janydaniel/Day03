0589540c6bf75f68e5fcaa30d1a7f6cf4b0e9b8e
JD-janydaniel <janydaniel59@gmail.com>
2024-01-28 12:50:23 +0530

    day03 task uploaded

--- script1.js
+++ script1.js
@@ -1,8 +1,8 @@
 // Question no:1
 // How to compare two JSON have the same properties without order?
 // solution
-var obj1 = {name:"jany daniel",age:27};
-var obj2 = {age:27,name:"jany daniel"};
+var obj1 = {"name":"jany daniel","age":27};
+var obj2 = {"age":27,"name":"jany daniel"};
 var result=true;
 
 if(Object.keys(obj1).length==Object.keys(obj2).length){

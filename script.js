066a3e380bfeb80ef6d6b4f4e482f3b17f1ed7ed
JD-janydaniel <janydaniel59@gmail.com>
2024-01-28 11:39:03 +0530

    day03 task

---
+++ script.js
@@ -0,0 +1,3 @@
+// Question no:2
+// Use the rest countries API URL->https://restcountries.com/v3.1/all and diplay all the contries flag in the console
+// Solution
---
+++ script1.js
@@ -0,0 +1,3 @@
+// Question no:1
+// How to compare two JSON have the same properties without order?
+// solution
